e457a50af7ba6e3d586619af9d7d0e978cc46180
handlename <nagata@handlena.me>
2016-06-07 11:31:28 +0900

    Layer1切り替えキーの位置を移動
    ホームポジションから動かずに切り替えたい

--- keyboard/ergodox_ez/keymaps/original/keymap.c
+++ keyboard/ergodox_ez/keymaps/original/keymap.c
@@ -16,53 +16,98 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | +L2  |           | +L2  |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------| +L1  |           | +L1  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| Esc  |           | Ent  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LAlt | Left | Right|   `  |   \  |                                       |   [  |   ]  | Down |  Up  |  RAlt  |
  *   `----------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Home | Ent  |       | PgUp | PgDn |
+ *                                        | Home | End  |       | PgUp | PgDn |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      | Tab  |       | Tab  |      |      |
  *                                 | LGui | Space|------|       |------|Sft/Bs| RGui |
- *                                 |      |      | Esc  |       | Ent  |      |      |
+ *                                 |      |      | +L1  |       | +L1  |      |      |
  *                                 `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [L_BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESCAPE,  KC_1,     KC_2,     KC_3,     KC_4,    KC_5,      KC_6,
-        KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,    KC_T,      MO(L_SYST),
-        KC_LCTRL,   KC_A,     KC_S,     KC_D,     KC_F,    KC_G,
-        KC_LSHIFT,  KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,      MO(L_FUNC),
+        KC_ESCAPE,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      MO(L_SYST),
+        KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      MO(L_MOUS),
+        KC_LCTRL,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
+        KC_LSHIFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,      KC_ESCAPE,
         KC_LALT,    KC_LEFT,  KC_RGHT,  KC_GRAVE, KC_BSLASH,
 
-                                                           KC_HOME,   KC_END,
-                                                                      KC_TAB,
-                                                 KC_LGUI,  KC_SPC,    KC_ESC,
+                                                            KC_HOME,   KC_END,
+                                                                       KC_TAB,
+                                                  KC_LGUI,  KC_SPC,    MO(L_SYMB),
 
-        // ritht hand
-        KC_5,       KC_6,     KC_7,     KC_8,    KC_9,     KC_0,      KC_MINS,
-        MO(L_SYST), KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,      KC_EQUAL,
-                    KC_H,     KC_J,     KC_K,    KC_L,     KC_SCOLON, KC_QUOTE,
-        MO(L_FUNC), KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH,   KC_RSFT,
-                              KC_LBRC,  KC_RBRC, KC_DOWN,  KC_UP,     KC_RALT,
+        // right hand
+        MO(L_SYST), KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,
+        MO(L_MOUS), KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_EQUAL,
+                    KC_H,     KC_J,     KC_K,     KC_L,     KC_SCOLON, KC_QUOTE,
+        KC_ENTER,   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   KC_RSFT,
+                              KC_LBRC,  KC_RBRC,  KC_DOWN,  KC_UP,     KC_RALT,
 
         KC_PGUP,    KC_PGDN,
         KC_TAB,
-        KC_ENT,     SFT_T(KC_BSPC),  KC_RGUI
+        MO(L_SYMB), SFT_T(KC_BSPC),  KC_RGUI
     ),
-/* Keymap 1: Function Layer
+/* Keymap 1: Symbols & Numbers Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |    *   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |    *   |      |      |  Up  |      |      |      |           |      |      | LClk | MsU  | RClk |      |        |
+ * |    *   |      |      |   `  |   \  |      |  *   |           |  *   |      |   [  |   ]  |   -  |   =  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |    *   |   1  |   2  |   3  |   4  |   5  |------|           |------|   6  |   7  |   8  |   9  |   0  |        |
+ * |--------+------+------+------+------+------|  *   |           |  *   |------+------+------+------+------+--------|
+ * |    *   |      |      |      |      |      |      |           |      |      |      |      |      |      |    *   |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |   *  |      |      |      |      |                                       |      |      |      |      |   *  |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |  *   |  *   |       |   *  |   *  |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |  *   |       |   *  |      |      |
+ *                                 |  *   |  *   |------|       |------|   *  |   *  |
+ *                                 |      |      |  *   |       |   *  |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// Symbols & Numbers keys
+[L_SYMB] = KEYMAP(
+       // left hand
+       KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,     KC_F6,    KC_TRNS,
+       KC_TRNS, KC_NO,   KC_NO,   KC_GRAVE, KC_BSLASH, KC_NO,    KC_TRNS,
+       KC_TRNS, KC_1,    KC_2,    KC_3,     KC_4,      KC_5,
+       KC_TRNS, KC_NO,   KC_NO,   KC_NO,    KC_NO,     KC_NO,    KC_TRNS,
+       KC_TRNS, KC_NO,   KC_NO,   KC_NO,    KC_NO,
+
+                                                       KC_TRNS,  KC_TRNS,
+                                                                 KC_TRNS,
+                                            KC_TRNS,   KC_TRNS,  KC_TRNS,
+
+       // right hand
+       KC_TRNS, KC_F7,   KC_F8,   KC_F9,    KC_F10,    KC_F11,   KC_F12,
+       KC_TRNS, KC_NO,   KC_LBRC, KC_RBRC,  KC_MINS,   KC_EQUAL, KC_NO,
+                KC_6,    KC_7,    KC_8,     KC_9,      KC_0,     KC_NO,
+       KC_TRNS, KC_NO,   KC_NO,   KC_NO,    KC_NO,     KC_NO,    KC_TRNS,
+                         KC_NO,   KC_NO,    KC_NO,     KC_NO,    KC_TRNS,
+
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS
+),
+/* Keymap 2: Mouse & Cursor Layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |    *   |      |      |      |      |      |  *   |           |  *   |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |    *   |      |      |  Up  |      |      |  *   |           |  *   |      | LClk | MsU  | RClk |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |    *   |      | Left | Down | Right|      |------|           |------|      | MsL  | MsD  | MsR  |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  *   |           |  *   |------+------+------+------+------+--------|
  * |    *   |      |      |      |      |      |      |           |      |      |      |      |      |      |    *   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |   *  |      |      |      |      |                                       |      |      |      |      |   *  |
