bfdde565927928ae48b8289d3360a4244c82df33
handlename <nagata@handlena.me>
2016-08-30 14:12:07 +0900

    Escは一般的な位置にあったほうが使いやすい

--- keyboard/ergodox_ez/keymaps/original/keymap.c
+++ keyboard/ergodox_ez/keymaps/original/keymap.c
@@ -11,13 +11,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Base layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | `      |   1  |   2  |   3  |   4  |   5  | Esc  |           | +L3  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * | Esc    |   1  |   2  |   3  |   4  |   5  | +L3  |           |  `   |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | +L2  |           | +L2  |   Y  |   U  |   I  |   O  |   P  |   [    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|  B   |           | Ent  |------+------+------+------+------+--------|
- * | LShift |LShift|   Z  |   X  |   C  |   V  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | LShift |   Z  |   Z  |   X  |   C  |   V  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LAlt | Back |  Fwd | Left | Right|                                       | Down |  Up  | VolD | VolU |  RAlt  |
  *   `----------------------------------'                                       `------------------------------------'
@@ -31,10 +31,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [L_BASE] = KEYMAP(
         // left hand
-        KC_GRAVE,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_ESC,
+        KC_ESC,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      MO(L_SYST),
         KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      MO(L_MOUS),
         KC_LCTL,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
-        KC_LSFT,    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,
+        KC_LSFT,    KC_Z,     KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,
         KC_LALT,    KC_WBAK,  KC_WFWD,  KC_LEFT,  KC_RGHT,
 
                                                             KC_HOME,   KC_END,
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                   KC_LGUI,  KC_SPC,    MO(L_SYMB),
 
         // right hand
-        MO(L_SYST), KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,
+        KC_GRAVE,   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,
         MO(L_MOUS), KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,
                     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCOLON, KC_QUOTE,
         KC_ENT,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   KC_RSFT,
