d2d521fbf1cbc1c7273864d3cf5a7e49753432ea
handlename <nagata@handlena.me>
2020-06-21 22:39:21 +0900

    SandS

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/original/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/original/keymap.c
@@ -12,6 +12,8 @@
 #define LOWER MO(_LOWER)
 #define GAMING TG(_GAMING)
 
+#define MT_SNS MT(MOD_LSFT, KC_SPACE) // SandS
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_5x6(
@@ -20,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LCTL,KC_A   ,KC_O   ,KC_E   ,KC_U   ,KC_I   ,                        KC_D   ,KC_H   ,KC_T   ,KC_N   ,KC_S   ,KC_MINS,
      KC_LSFT,KC_SCLN,KC_Q   ,KC_J   ,KC_C   ,KC_X   ,                        KC_B   ,KC_M   ,KC_W   ,KC_V   ,KC_Z   ,GAMING ,
                      KC_LEFT,KC_RGHT,                                                        KC_DOWN,KC_UP  ,
-                                             KC_LGUI,KC_SPC ,        KC_BSPC,KC_RGUI,
+                                             KC_LGUI,MT_SNS ,        KC_BSPC,KC_RGUI,
                                    LT(RAISE, KC_ESC),XXXXXXX,        XXXXXXX,LT(RAISE, KC_ENT),
                                              KC_LALT,LOWER  ,        LOWER  ,KC_RALT
   ),
