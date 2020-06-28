54d6bd5719ae4d7b7ae2457c007e2b5335145c23
handlename <nagata@handlena.me>
2020-06-28 18:57:53 +0900

    GAMINGレイヤーではGUIキー無効
    =Winキー無効

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/original/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/original/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_G   ,KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,                        KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_COLN,_______,
      KC_B   ,KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,                        KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,_______,
                      _______,_______,                                                        _______,_______,
-                                             _______,KC_SPC ,        _______,_______,
+                                             XXXXXXX,KC_SPC ,        _______,XXXXXXX,
                                              _______,_______,        _______,_______,
                                              _______,_______,        _______,_______
   ),
