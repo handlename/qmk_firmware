21f7b78ac57847f017db68cbaccd3f7a23543da6
handlename <nagata@handlena.me>
2024-10-11 01:14:36 +0900

    add original layout for atreus62

---
+++ keyboards/atreus62/keymaps/original/keymap.c
@@ -0,0 +1,51 @@
+// this is the style you want to emulate.
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+
+#include QMK_KEYBOARD_H
+
+// LAYERS
+
+#define _BASE 0
+#define _LOWER 1
+#define _RAISE 2
+
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+
+// KEY MACROS
+
+#define MT_SNS MT(MOD_LSFT, KC_SPACE) // SandS
+#define MT_RNB MT(MOD_LALT, KC_BSPC)
+
+// KEY COMBO
+
+const uint16_t PROGMEM ecs_combo[] = {KC_H, KC_T, COMBO_END};
+combo_t key_combos[] = {
+  COMBO(ecs_combo, KC_ESC),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT( /* dvorak+ */
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL  ,
+    KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                                         KC_F,    KC_G,    KC_K,    KC_R,    KC_L,    KC_SLSH ,
+    KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                                         KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS ,
+    KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_C,    KC_X,                                         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT ,
+    LOWER,   XXXXXXX, KC_LEFT, KC_RGHT, KC_LGUI, MT_SNS, LT(RAISE, KC_ESC), LT(RAISE, KC_ENT), MT_RNB, KC_RGUI, KC_DOWN, KC_UP,   XXXXXXX, LOWER
+  ),
+
+  [_RAISE] = LAYOUT(
+     KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                 KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,
+     _______,_______,_______,_______,_______,_______,                 _______,KC_LBRC,KC_RBRC,_______,KC_PIPE,KC_BSLS,
+     _______,_______,_______,_______,_______,_______,                 _______,KC_LCBR,KC_RCBR,_______,KC_PLUS,KC_EQL ,
+     _______,_______,_______,_______,_______,_______,                 _______,KC_9   ,KC_0   ,_______,_______,_______,
+     _______,_______,_______,_______,_______,_______,_______, _______,_______,_______,_______,_______,_______,_______
+  ),
+
+  [_LOWER] = LAYOUT(
+     _______,_______,_______,_______,_______,_______,                 KC_MUTE,_______,_______,_______,_______,_______,
+     _______,_______,_______,KC_UP  ,_______,KC_VOLD,                 KC_VOLU,KC_P1  ,KC_P2  ,KC_P3  ,_______,_______,
+     _______,_______,KC_LEFT,KC_DOWN,KC_RGHT,KC_BRID,                 KC_BRIU,KC_P4  ,KC_P5  ,KC_P6  ,_______,_______,
+     _______,_______,_______,_______,_______,_______,                 _______,KC_P7  ,KC_P8  ,KC_P9  ,_______,_______,
+     _______,_______,_______,_______,_______,_______,_______, _______,_______,_______,_______,_______,_______,QK_BOOT
+  )
+};
